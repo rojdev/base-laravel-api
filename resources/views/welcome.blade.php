d5ae1c5dad4dfc6b0ef694631347ddf013010a3b
rojdev <rojdev4@gmail.com>
2025-07-07 16:16:20 -0400

    base api clean of almost all frontend dependencies

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+</head>
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+        background-color: #414141;
+        color: #374151;
+        text-align: center;
+        padding: 50px;
+    }
+    h1 {
+        font-size: 3rem;
+        color: #2563eb;
+    }
+    p {
+        font-size: 1.25rem;
+        color: #6b7280;
+    }
+    a {
+        color: #2563eb;
+        text-decoration: none;
+    }
+</style>
+<body>
+    <h1>API Working</h1>
+</body>
+</html>
